d9415780986d10ba9f7d1dfa250f1dede49c8525
Peter Witwer <pwitwer@icloud.com>
2021-02-17 21:09:26 -0700

    Update app.js
    progress

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -6,26 +6,52 @@ d3.json("samples.json").then((data) => {
             y: data.otu_ids,
             type: 'bar',
             orientation: 'h',
-            text: data.out_labels,
+            text: data.otu_labels,
             marker: {color: "blue"},
             name: "Top 10 OTUs"
         }
     ];
 
-    var data = trace1;
+    var data = [trace1];
 
+      // Define the plot layout
+    var layout = {
+        title: "Top 10 OTUs",
+        xaxis: { title: "OTU number/type" },
+        yaxis: { title: "Prevelance" }
+    };
 
+    // Plot the chart to a div tag with id "plot"
+    Plotly.newPlot("bar", data, layout);
 
+    // Call updatePlotly() when a change takes place to the DOM
+    d3.selectAll("#selDataset").on("change", updatePlotly);
 
+    // This function is called when a dropdown menu item is selected
+    function updatePlotly() {
+        // Use D3 to select the dropdown menu
+        var dropdownMenu = d3.select("#selDataset");
+        // Assign the value of the dropdown menu option to a variable
+        var dataset = dropdownMenu.property("value");
 
+        // Initialize x and y arrays
+        var x = [];
+        var y = [];
 
+        if (dataset === 'dataset1') {
+            x = [1, 2, 3, 4, 5];
+            y = [1, 2, 4, 8, 16];
+        }
 
+        else if (dataset === 'dataset2') {
+            x = [10, 20, 30, 40, 50];
+            y = [1, 10, 100, 1000, 10000];
+        }
 
+        // Note the extra brackets around 'x' and 'y'
+        Plotly.restyle("bar", "x", [x]);
+        Plotly.restyle("bar", "y", [y]);
+    }
 
-
-
-
-
-
-
+init();
 })
