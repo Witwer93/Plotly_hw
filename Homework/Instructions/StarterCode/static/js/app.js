7aa42f12db047c3cf53be52e52a048ce6b37cc29
Peter Witwer <pwitwer@icloud.com>
2021-02-17 22:32:04 -0700

    worked with TA, step 1 done
    created functions for adjusting chart based on dropdown selection

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -1,18 +1,56 @@
+function init()
+{
 d3.json("samples.json").then((data) => {
+
+    //grab names for dropdown
+    var names = data.names;
+    sampleName = names[0]
+
+    //
+    makeTrace(sampleName);
+    console.log(names);
+
+    //
+    var selector = d3.select("#selDataset")
+    //
+    names.forEach((sample) => {
+        selector.append("option").text(sample).property("value", sample)
+    });
+    })
+}
+    function makeTrace(dataset){
+
+        d3.json("samples.json").then((data) => {
+
+        var filterData = data;
+
+        filterData = data.samples;
+        
+        //
+        var resultArray = filterData.filter(sampleObj => sampleObj.id == dataset);
+        var result = resultArray[0];
+        console.log(result)
+        //
+        var otu_ids = result.otu_ids;
+        var otu_labels = result.otu_labels;
+        var sample_values = result.sample_values;
+        var yticks = otu_ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse();
+        var xdata = sample_values.slice(0, 10).reverse();
+        console.log(yticks, xdata, otu_labels)
     
-    var trace1 = [
+        var trace1 = [
         {
-            x: data.sample_values,
-            y: data.otu_ids,
+            x: xdata,
+            y: yticks,
             type: 'bar',
             orientation: 'h',
-            text: data.otu_labels,
+            text: otu_labels,
             marker: {color: "blue"},
             name: "Top 10 OTUs"
         }
     ];
 
-    var data = [trace1];
+    var data2 = trace1;
 
       // Define the plot layout
     var layout = {
@@ -21,37 +59,15 @@ d3.json("samples.json").then((data) => {
         yaxis: { title: "Prevelance" }
     };
 
+
     // Plot the chart to a div tag with id "plot"
-    Plotly.newPlot("bar", data, layout);
-
-    // Call updatePlotly() when a change takes place to the DOM
-    d3.selectAll("#selDataset").on("change", updatePlotly);
-
+    Plotly.newPlot("bar", data2, layout);
+        });
+    };
     // This function is called when a dropdown menu item is selected
-    function updatePlotly() {
-        // Use D3 to select the dropdown menu
-        var dropdownMenu = d3.select("#selDataset");
-        // Assign the value of the dropdown menu option to a variable
-        var dataset = dropdownMenu.property("value");
 
-        // Initialize x and y arrays
-        var x = [];
-        var y = [];
-
-        if (dataset === 'dataset1') {
-            x = [1, 2, 3, 4, 5];
-            y = [1, 2, 4, 8, 16];
-        }
-
-        else if (dataset === 'dataset2') {
-            x = [10, 20, 30, 40, 50];
-            y = [1, 10, 100, 1000, 10000];
-        }
-
-        // Note the extra brackets around 'x' and 'y'
-        Plotly.restyle("bar", "x", [x]);
-        Plotly.restyle("bar", "y", [y]);
-    }
+function optionChanged(dataset) {
+    makeTrace(dataset);
+};
 
 init();
-})
