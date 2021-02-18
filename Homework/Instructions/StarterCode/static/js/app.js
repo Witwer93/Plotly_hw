1617e6895a03c4e31942492f0df876a838685a26
Peter Witwer <pwitwer@icloud.com>
2021-02-17 20:45:05 -0700

    basic setup
    getting syntaxError: Unexpected token ':'
    but it's just the file I downloaded.

--- Homework/Instructions/StarterCode/static/js/app.js
+++ Homework/Instructions/StarterCode/static/js/app.js
@@ -1,6 +1,18 @@
 d3.json("samples.json").then((data) => {
     
+    var trace1 = [
+        {
+            x: data.sample_values,
+            y: data.otu_ids,
+            type: 'bar',
+            orientation: 'h',
+            text: data.out_labels,
+            marker: {color: "blue"},
+            name: "Top 10 OTUs"
+        }
+    ];
 
+    var data = trace1;
 
 
 
